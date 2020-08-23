ff0f3a4c77b8d7bf6fbbad0215ff39dcf84c0286
TannakornM <maneepak110@gmail.com>
2020-08-23 16:51:10 +0700

    part5

---
+++ screen/WeatherScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import Weather from '../components/Weather'
+
+export default function WeatherScreen({route}){
+
+    return(
+        <View>
+            <Weather zipCode={route.params.zipCode} />
+            <StatusBar style="auto" />
+        </View>
+
+
+    )
+}
